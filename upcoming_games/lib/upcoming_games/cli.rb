940ce18f5e01a9dc5be4e19197a89e39b70d7983
Travis Clarkson <tk1667@gmail.com>
2018-03-05 16:48:43 +0000

    started #list_games, #menu, and #goodbye

--- upcoming_games/lib/upcoming_games/cli.rb
+++ upcoming_games/lib/upcoming_games/cli.rb
@@ -1,7 +1,38 @@
 class UpcomingGames::CLI
 
   def call
-    puts "Hello World!"
+    list_games
+    menu
+    goodbye
+  end
+
+  def list_games
+    puts "Ten Upcoming PC Games"
+    puts <<-DOC.gsub /^\s*/, ''
+    1. Super Seducer - Simulation - Red Dahlia Interactive - March 6, 2018
+    2. Final Fantasy XV - Action, RPG - Square Enix - March 6, 2018
+    3. Fear Effect Sedna - Action, Adventure - Sushee - March 6, 2018
+    DOC
+  end
+
+  def menu
+    puts "Enter number for game you want more information on:"
+    input = nil
+    while input != "exit"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "Game 1 description"
+      when "2"
+        puts "Game 2 description"
+      when "3"
+        puts "Game 3 description"
+      end
+    end
+  end
+
+  def goodbye
+    puts "Check back soon for the newest game releases."
   end
 
 end
