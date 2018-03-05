fe3b0fdac087535e628f8037819a9da8903ca8fa
Travis Clarkson <tk1667@gmail.com>
2018-03-05 22:17:52 +0000

    update #self.scrape_upcoming_list to get details

--- upcoming_games/lib/upcoming_games/scraper.rb
+++ upcoming_games/lib/upcoming_games/scraper.rb
@@ -3,7 +3,16 @@ class UpcomingGames::Scraper
 
   def self.scrape_upcoming_list
     doc = Nokogiri::HTML(open("http://www.ign.com/upcoming/games/pc"))
-    games = doc.css("div.clear gameList-gameShort")
+    games = doc.css("div.clear.gameList-gameShort")
+    games.collect do |game|
+      {
+        :title =>game.css("div.game-title h3 a").text.strip,
+        :genre =>game.css("div.game-details strong").text.gsub(' ','').strip,
+        :publisher =>game.css("div.publisher.grid_3").text.strip,
+        :release_date =>game.css("div.releaseDate.grid_3.omega").text.strip,
+        :url =>game.css("div.game-title h3 a").attribute("href").value
+      }
+    end
     binding.pry
   end
 
